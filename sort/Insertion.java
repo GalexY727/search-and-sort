c681037241ad8974ae8b0989e2b7dab35fecb1c7
Alexander Hamilton <alexander.hamilton@mrjaffesclass.com>
2022-09-26 12:50:25 -0700

    Insertion Sort

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -16,7 +16,17 @@ public class Insertion
    */
   public static int[] sort(int[] arr) {
     // Your algorithm goes here!
-
+    for (int i = 1; i < arr.length; i++)
+    {
+        int j = i;
+        while (arr[j] < arr[j-1])
+        {
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
+    }
   }
   
   public static void main(String[] args) {
